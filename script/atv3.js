b8ba098c144cb6f8e17322e2d9dae896b2621c19
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2023-03-20 02:00:14 -0300

    att

--- script/atv3.js
+++
@@ -1,9 +0,0 @@
-const botao = document.querySelector('[botao]')
-const lista = document.querySelector('[lista]')
-const tipo = document.querySelector('[tipo]')
-
-function mostraTintas(){
-    if (tipo.dataset = 'tinta-exterior'){
-        lista.style.display = 'none'
-    }
-}
