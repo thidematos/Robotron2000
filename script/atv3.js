1277a04d3ae8a3e5f26f25c991c23287b234443b
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2023-03-20 02:01:56 -0300

    Revert "att"
    This reverts commit b8ba098c144cb6f8e17322e2d9dae896b2621c19.

---
+++ script/atv3.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector('[botao]')
+const lista = document.querySelector('[lista]')
+const tipo = document.querySelector('[tipo]')
+
+function mostraTintas(){
+    if (tipo.dataset = 'tinta-exterior'){
+        lista.style.display = 'none'
+    }
+}
