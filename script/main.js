606d20f80652793a3a4122dff4451c9d3484757b
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2023-03-20 00:32:26 -0300

    att

--- script/main.js
+++ script/main.js
@@ -1,24 +1,56 @@
-const braco = document.querySelector('#braco');
 
-const controle = document.querySelectorAll('.controle-ajuste')
+    const atributos = {
+        "bracos": {
+            "forca": 29,
+            "poder": 35,
+            "energia": -21,
+            "velocidade": -5
+        },
+        "blindagem": {
+            "forca": 41,
+            "poder": 20,
+            "energia": 0,
+            "velocidade": -20
+        },
+        "nucleos":{
+            "forca": 0,
+            "poder": 7,
+            "energia": 48,
+            "velocidade": -4
+        },
+        "pernas":{
+            "forca": 27,
+            "poder": 21,
+            "energia": -32,
+            "velocidade": 43
+        },
+        "foguetes":{
+            "forca": 0,
+            "poder": 28,
+            "energia": 0,
+            "velocidade": -2
+        }
+    }
+
+const controle = document.querySelectorAll('[data-controle]')
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]');
     if (operacao === "-") {
-        if (parseInt(braco.value)>0) {
-            braco.value = parseInt(braco.value) - 1
+        if (parseInt(peca.value)>0) {
+            peca.value = parseInt(peca.value) - 1
         }
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
 
-var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"]; 
 
 
  
